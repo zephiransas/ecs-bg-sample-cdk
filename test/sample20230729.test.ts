e723f9d5e4014127732226302e23f41a07c01cf9
Takafumi Yoshida <yoshida.takafumi@classmethod.jp>
2023-07-27 13:39:15 +0900

    Initial commit

---
+++ test/sample20230729.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Sample20230729 from '../lib/sample20230729-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sample20230729-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Sample20230729.Sample20230729Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
